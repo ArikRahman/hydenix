dbfadd8f9b48fa43203cde6b0dbd508384ddb6aa
false <false>
2026-01-10 18:00:45 -0600

    Disable nvidia prime

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
     # Run `lshw -short` or `lspci` to identify your hardware
 
     # GPU Configuration (choose one):
-    inputs.nixos-hardware.nixosModules.common-gpu-nvidia # NVIDIA
+    # inputs.nixos-hardware.nixosModules.common-gpu-nvidia # NVIDIA
     # inputs.nixos-hardware.nixosModules.common-gpu-amd # AMD
 
     # CPU Configuration (choose one):
